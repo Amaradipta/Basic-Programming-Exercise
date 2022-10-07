4aaf2f3a37d4ea3adc1262a06ac5e889a01c66c5
Amaradipta <114727662+Amaradipta@users.noreply.github.com>
2022-10-07 15:45:10 +0700

    update

---
+++ BiodataApp.py
@@ -0,0 +1,19 @@
+name = input("Your Name: ")
+Class = input("Your Class: ")
+major = input("Your Major: ")
+quote = input("your quote: ")
+age = int(input("your age: "))
+iLikespiceFood = bool(input("I Like Spice Food: "))
+hobbies = input("your hobbies: ").split(',')
+
+
+print(f"My name is: {name}\nClass: {Class}\nMajor: {major}\nQuote: {quote}\nAge: {age}\niLikespiceFood: {iLikespiceFood}\nHobbies: {hobbies}  ")
+
+print("="*50)
+print("nama : ",type(name))
+print("class : ",type(Class))
+print("major : ",type(major))
+print("quote : ",type(major))
+print("age : ",type(age))
+print("i like spice food : ",type(iLikespiceFood))
+print("hobbies : ",type(hobbies))
